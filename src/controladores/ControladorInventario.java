fc0d7e8d8acffa39803375a075ca2a5727cd69e3
KSGAMER <rogelio.gonzalez@alumno.uttehuacan.edu.mx>
2020-03-27 11:20:46 -0600

    Inventario por fin jajaja

---
+++ src/controladores/ControladorInventario.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import javax.swing.table.DefaultTableModel;
+import modelos.ModeloInventario;
+
+/**
+ *
+ * @author KSGAMER
+ */
+public class ControladorInventario extends ModeloInventario{
+    public DefaultTableModel tablaInventario(String filtro) {
+        return cargarTabla(filtro);
+    }
+    
+    public void insertarInventario(String producto, int cantidad, String observaciones) {
+        insertInventario(producto, cantidad, observaciones);
+    }
+    
+    public void actualizarInventario(String producto, int cantidad, String observaciones, int id) {
+        updateInventario(producto, cantidad, observaciones, id);
+    }
+    
+    public void eliminarInventario(int id) {
+        deleteInventario(id);
+    }
+}
